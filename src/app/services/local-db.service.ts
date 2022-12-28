0ada78a8f424fc2fbdbd159530175e30e90ec176
Vladislav Indrey <v.indrej@nv.dunice.net>
2022-12-28 22:53:16 +0300

    Add todos comments

--- src/app/services/local-db.service.ts
+++ src/app/services/local-db.service.ts
@@ -18,4 +18,7 @@ export class LocalDbService extends Dexie {
       tasks: '++id, taskListId',
     });
   }
+
+  // TODO:  liveQuery<> return observable<> https://dexie.org/docs/Tutorial/Angular (get)
+  // TODO: async db.add()
 }
