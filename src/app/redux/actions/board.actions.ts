15007326bd52dd6d2db2428c84ad2fc918348385
Vlad_Indrey <v.indrej@nv.dunice.net>
2022-12-28 12:28:25 +0300

    Create list actions

---
+++ src/app/redux/actions/board.actions.ts
@@ -0,0 +1,5 @@
+import {createAction} from "@ngrx/store";
+
+export const createList = createAction('[Board Component] Create List');
+export const removeList = createAction('[Board Component] Remove List');
+export const moveList = createAction('[Board Component] Move List');
