6d8850f31bd17f870108c3f59cf83454e7cc6340
Vlad_Indrey <v.indrej@nv.dunice.net>
2023-01-12 09:27:46 +0300

    Add list refactoring

--- src/app/components/add-list/add-list.component.ts
+++ src/app/components/add-list/add-list.component.ts
@@ -1,17 +1,23 @@
 import {ChangeDetectorRef, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
 import {Store} from "@ngrx/store";
+
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
 
-import {addList, loadLists} from "../../redux/actions/list.actions";
 import {BlueInputDirective} from "../../shared/blue-input.directive";
+import {addList} from "../../redux/actions/list.actions";
 
 // TODO: Add blur event and validation.
 @Component({
   selector: 'app-add-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, BlueInputDirective],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    BlueInputDirective,
+  ],
   templateUrl: './add-list.component.html',
   styleUrls: ['./add-list.component.scss']
 })
@@ -33,8 +39,7 @@ export class AddListComponent {
   public onAddClicked($event: MouseEvent, title: string): void {
     $event.stopPropagation();
 
-    this.store.dispatch(addList({list: {title}})); // TODO: Load lists on add list
-    this.store.dispatch(loadLists());
+    this.store.dispatch(addList({list: {title}}));
     this.isAddMod = false;
   }
 
