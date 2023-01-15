da372d4b218911f784c6dc17cbf4596e68f756d6
Vladislav Indrey <v.indrej@nv.dunice.net>
2023-01-15 16:37:55 +0300

    Create add card tests

--- src/app/components/add-card/add-card.component.spec.ts
+++ src/app/components/add-card/add-card.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AddCardComponent } from './add-card.component';
+import {AddCardComponent} from './add-card.component';
 import {provideMockStore} from "@ngrx/store/testing";
 
 describe('AddCardComponent', () => {
@@ -9,10 +9,10 @@ describe('AddCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ AddCardComponent ],
+      imports: [AddCardComponent],
       providers: [provideMockStore({})]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AddCardComponent);
     component = fixture.componentInstance;
@@ -21,5 +21,54 @@ describe('AddCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#onAddListClicked() should correctly change boolean value #isAddMod', () => {
+    expect(component.isAddMod)
+      .withContext('false at first')
+      .toBeFalse();
+    component.onAddListClicked();
+    expect(component.isAddMod)
+      .withContext('true after click')
+      .toBeTruthy();
+  });
+
+  it('add-card should correctly change boolean value #isAddMod', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const addCardDiv: HTMLDivElement | null = element.querySelector('.add-card');
+    expect(component.isAddMod)
+      .withContext('false at first')
+      .toBeFalse();
+    expect(addCardDiv !== null).toBeTruthy();
+    if (addCardDiv) {
+      addCardDiv.click();
+    }
+    expect(component.isAddMod)
+      .withContext('true after click')
+      .toBeTruthy();
+  });
+
+  it('#onCancelClicked() should change boolean value #isAddMod', () => {
+    const event = new MouseEvent('clear-button__button', {bubbles: true});
+    component.isAddMod = true;
+    expect(component.isAddMod)
+      .withContext('true after click')
+      .toBeTruthy();
+    component.onCancelClicked(event);
+    expect(component.isAddMod)
+      .withContext('false at first')
+      .toBeFalse();
+  });
+
+  it('#onAddClicked() should change boolean value #isAddMod ', function () {
+    const event = new MouseEvent('clear-button__button', {bubbles: true});
+    component.isAddMod = true;
+    expect(component.isAddMod)
+      .withContext('true after click')
+      .toBeTruthy();
+    component.onAddClicked(event, 'sample string', 'sample string');
+    expect(component.isAddMod)
+      .withContext('false at first')
+      .toBeFalse();
   });
 });
