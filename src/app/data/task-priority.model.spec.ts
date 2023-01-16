c2987de18d93597491539c357555d05d7524930c
VladislavIndrey <v.indrej@nv.dunice.net>
2023-01-16 15:57:37 +0300

    Create task priority model unit tests

---
+++ src/app/data/task-priority.model.spec.ts
@@ -0,0 +1,15 @@
+import {TaskPriorityModel} from "./task-priority.model";
+
+describe('TaskPriorityModel', () => {
+  const taskPriorityModel = new TaskPriorityModel();
+
+  it('#get priorities() should return correctly value', () => {
+    const result = taskPriorityModel.priorities;
+    expect(result.length).toEqual(10);
+  });
+
+  it('#changePriority() should throw Error if id is incorrect',  () => {
+    const id = -1;
+    expect(() => taskPriorityModel.changePriority(id)).toThrowError(`No priority was found with id: ${id}`);
+  });
+});
