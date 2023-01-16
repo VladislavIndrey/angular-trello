be899e3449e16fcf929d9301c0a3325e17bd2ff6
VladislavIndrey <v.indrej@nv.dunice.net>
2023-01-16 15:00:54 +0300

    Create card component unit tests

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -2,13 +2,14 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CardComponent} from './card.component';
 import {MockStore, provideMockStore} from "@ngrx/store/testing";
-import {taskInitialState, taskReducer} from "../../infrastructure/redux/reducers/task.reducer";
-import {StoreModule} from "@ngrx/store";
+import {taskInitialState} from "../../infrastructure/redux/reducers/task.reducer";
+import {ITask} from "../../data/db/task";
 
 describe('CardComponent', () => {
+  const mockTask: ITask = {id: 1, text: 'mock', ownerName: 'mock', priority: 1, taskListId: 1};
   let store: MockStore;
-  const initialState: taskInitialState = {
-    tasks: [{taskListId: 0, text: 'a', ownerName: 'a', priority: 0}],
+  const mockInitialState: taskInitialState = {
+    tasks: [mockTask],
     error: null,
     isLoading: false
   };
@@ -18,7 +19,7 @@ describe('CardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CardComponent],
-      providers: [provideMockStore({ initialState})]
+      providers: [provideMockStore({initialState: mockInitialState})]
     })
       .compileComponents();
 
@@ -28,9 +29,50 @@ describe('CardComponent', () => {
     store = TestBed.inject(MockStore);
   });
 
+  beforeEach(() => {
+    component.task = mockTask;
+    component.tasks = [mockTask];
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should change priority when task is not undefined', () => {
+    component.ngOnInit();
+    expect(component.taskPriorityModel.priority.id).toEqual(1);
+  });
 
+  it('#onEditClicked() should change #isEditMode correctly', function () {
+    expect(component.isEditMode)
+      .withContext('false at first')
+      .toBeFalse();
+    component.onEditClicked();
+    expect(component.isEditMode)
+      .withContext('true after click')
+      .toBeTruthy();
+  });
+
+  it('#onCancelClicked() should change #isEditMode correctly', function () {
+    component.isEditMode = true;
+    expect(component.isEditMode)
+      .withContext('true at first')
+      .toBeTruthy();
+    component.onCancelClicked();
+    expect(component.isEditMode)
+      .withContext('false after click')
+      .toBeFalse();
+  });
+
+  it('#onDeleteTaskClicked() should call #cardModel.deleteTask()', () => {
+    spyOn(component['cardModel'], 'deleteTask');
+    component.onDeleteTaskClicked();
+    expect(component['cardModel'].deleteTask).toHaveBeenCalled();
+  });
+
+  it('#onSaveClicked() should call #cardModel.updateTask()', () => {
+    spyOn(component['cardModel'], 'updateTask');
+    component.onSaveClicked('mock', 'mock');
+    expect(component['cardModel'].updateTask).toHaveBeenCalled();
+  });
 });
