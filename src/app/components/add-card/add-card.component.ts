e9116df1b0b219cf048f7888e64be5b6bed69081
VladislavIndrey <v.indrej@nv.dunice.net>
2023-01-18 16:55:27 +0300

    Add card component refactoring

--- src/app/components/add-card/add-card.component.ts
+++ src/app/components/add-card/add-card.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Store} from "@ngrx/store";
 
