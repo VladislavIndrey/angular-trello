fa043c68fd50fb454614aae3a1705aace87e1f59
VladislavIndrey <v.indrej@nv.dunice.net>
2023-01-20 10:52:02 +0300

    List actions refactoring

--- src/app/infrastructure/redux/actions/list.actions.ts
+++ src/app/infrastructure/redux/actions/list.actions.ts
@@ -1,26 +1,28 @@
-import {createAction, createActionGroup, props} from "@ngrx/store";
+import {createAction, props} from "@ngrx/store";
 
 import {IList} from "../../../data/db/list";
 
 const ADD_LIST_COMPONENT = '[Add List Component]';
 const BOARD_COMPONENT = '[Board Component]'
 const LOCAL_DB_API = '[Local DB API]';
+const MAIN_PAGE = '[Main Page]';
+const LIST_COMPONENT = '[List Component]';
 
-export const loadLists = createAction('[Main Page] Load Lists');
-export const listsLoaded = createAction('[Local DB API] Lists Loaded Success', props<{ taskLists: IList[] }>());
-export const listsLoadFailed = createAction('[Local DB API] Lists Load Failed', props<{ error: any }>());
+export const loadLists = createAction(`${MAIN_PAGE} Load Lists`);
+export const listsLoaded = createAction(`${LOCAL_DB_API} Lists Loaded Success`, props<{ taskLists: IList[] }>());
+export const listsLoadFailed = createAction(`${LOCAL_DB_API} Lists Load Failed`, props<{ error: any }>());
 
 export const addList = createAction(`${ADD_LIST_COMPONENT} Add List`, props<{ list: IList }>());
-export const listAdded = createAction('[Local DB API] List Added Success', props<{ lists: IList[] }>());
-export const listAddFailed = createAction('[Local DB API] List Add Failed', props<{ error: any }>());
+export const listAdded = createAction(`${LOCAL_DB_API} List Added Success`, props<{ lists: IList[] }>());
+export const listAddFailed = createAction(`${LOCAL_DB_API} List Add Failed`, props<{ error: any }>());
 
-export const updateList = createAction('[List Component] Edit List', props<{ id: number, list: IList }>());
-export const listUpdated = createAction('[Local DB API] List Updated', props<{ lists: IList[] }>());
-export const listUpdateFailed = createAction('[Local DB API] List Update Failed', props<{ error: any }>());
+export const updateList = createAction(`${LIST_COMPONENT} Edit List`, props<{ id: number, list: IList }>());
+export const listUpdated = createAction(`${LOCAL_DB_API} List Updated`, props<{ lists: IList[] }>());
+export const listUpdateFailed = createAction(`${LOCAL_DB_API} List Update Failed`, props<{ error: any }>());
 
-export const deleteList = createAction('[List Component] Delete List', props<{ list: IList }>());
-export const listDeleted = createAction('[Local DB API] List Deleted', props<{ lists: IList[] }>());
-export const deleteListFailed = createAction('[Local DB API] Delete List Failed', props<{ error: any }>());
+export const deleteList = createAction(`${LIST_COMPONENT} Delete List`, props<{ list: IList }>());
+export const listDeleted = createAction(`${LOCAL_DB_API} List Deleted`, props<{ lists: IList[] }>());
+export const deleteListFailed = createAction(`${LOCAL_DB_API} Delete List Failed`, props<{ error: any }>());
 
 export const moveList = createAction(`${BOARD_COMPONENT} Move List`, props<{ list: IList, currentIndex: number }>());
 export const listMoved = createAction(`${LOCAL_DB_API} List Moved`, props<{ lists: IList[] }>());
